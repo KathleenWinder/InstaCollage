7b057a3a1e3f523c7d37cbcb77b349c6a28a9734
akbashev <adzhalil@gmail.com>
2013-09-24 23:22:23 +0400

    Gettin sorted photos from server by user id

--- InstaCollage/src/main/java/ru/akbashev/instacollage/MainActivity.java
+++ InstaCollage/src/main/java/ru/akbashev/instacollage/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.akbashev.instacollage;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,9 +9,22 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.decode.BaseImageDecoder;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ClientConnectionManager;
@@ -26,30 +40,37 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 
 public class MainActivity extends Activity {
 
     //vars to work with url
     public static final String MAIN_URL = "https://api.instagram.com/v1/users/";
-    public static final int USER_ID = 0;
     public static final String MEDIA = "/media/recent/?";
     public static final String SEARCH = "search?q=";
-    public static final String ACCESS_TOKEN = "&access_token=7922048.f59def8.5f29380f2a5a4b38890670852004960d";
+    public static final String ACCESS_TOKEN = "&access_token=7922048.f59def8.5f29380f2a5a4b38890670852004960d&count=-1";
     public static final String CLIENT_ID = "&client_id=4361da965c654fafa36db6803d26a562";
 
     //vars for asynctask
     private static final int DEFAULT = 0;
     private static final int OK = 1;
     private static final int NO_USER = 2;
+    private ArrayList<InstaImages> instaImages;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
         final EditText editText = (EditText) findViewById(R.id.editText);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -118,6 +139,7 @@ public class MainActivity extends Activity {
                     Toast.makeText(MainActivity.this,"Mistake", Toast.LENGTH_SHORT).show();
                     break;
                 case OK:
+                    new GetImagesTask().execute(String.valueOf(mId));
                     Toast.makeText(MainActivity.this,"WELL DONE! User - " + mName + ", ID = " + mId, Toast.LENGTH_SHORT).show();
                     break;
                 case NO_USER:
@@ -126,5 +148,103 @@ public class MainActivity extends Activity {
             }
         }
     }
-    
-}
+
+    class GetImagesTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... name) {
+            try {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                HttpResponse response = null;
+                request = new HttpGet(MAIN_URL + name[0] + MEDIA  + ACCESS_TOKEN);
+                response = client.execute(request);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+                String json = reader.readLine();
+                JSONObject meta = new JSONObject(json).getJSONObject("meta");
+                JSONArray array = new JSONObject(json).getJSONArray("data");
+                if (meta.getInt("code") == 200){
+                    if (array.length() == 0)
+                        return NO_USER;
+                    else {
+                        instaImages = new ArrayList<InstaImages>();
+                        for (int i = 0; i < array.length(); i++){
+                            JSONObject current = array.getJSONObject(i);
+                            if (current.getString("type").equalsIgnoreCase("image")){
+                                instaImages.add(new InstaImages(current.getJSONObject("likes").getInt("count"),current.getJSONObject("images").getJSONObject("standard_resolution").getString("url")));
+                            }
+                        }
+                        Collections.sort(instaImages, new InstaComparator());
+                        return OK;
+                    }
+                } else
+                    return DEFAULT;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return DEFAULT;
+        }
+
+            @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            switch (result){
+                case DEFAULT:
+                    Toast.makeText(MainActivity.this,"Mistake", Toast.LENGTH_SHORT).show();
+                    break;
+                case OK:
+                    File cacheDir = StorageUtils.getCacheDirectory(MainActivity.this);
+                    ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(MainActivity.this)
+                            .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
+                            .discCacheExtraOptions(480, 800, Bitmap.CompressFormat.JPEG, 75, null)
+                            .threadPriority(Thread.NORM_PRIORITY - 1) // default
+                            .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+                            .denyCacheImageMultipleSizesInMemory()
+                            .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                            .memoryCacheSize(2 * 1024 * 1024)
+                            .memoryCacheSizePercentage(13) // default
+                            .discCache(new UnlimitedDiscCache(cacheDir)) // default
+                            .discCacheSize(50 * 1024 * 1024)
+                            .discCacheFileCount(100)
+                            .discCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+                            .imageDownloader(new BaseImageDownloader(MainActivity.this)) // default
+                            .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+                            .writeDebugLogs()
+                            .build();
+                    ImageLoader.getInstance().init(config);
+                    ImageLoader.getInstance().displayImage(instaImages.get(0).url,imageView);
+
+                    Toast.makeText(MainActivity.this,"WELL DONE!", Toast.LENGTH_SHORT).show();
+                    break;
+                case NO_USER:
+                    Toast.makeText(MainActivity.this,"No such user", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
+    public class InstaImages {
+
+        public String url;
+        public int count;
+
+        public InstaImages(int count, String url) {
+            this.url = url;
+            this.count = count;
+        }
+
+    }
+
+    public class InstaComparator implements Comparator<InstaImages> {
+
+        @Override
+        public int compare(InstaImages o1, InstaImages o2) {
+            return o2.count - o1.count;
+        }
+    }
+
+
+    }
--- InstaCollage/src/main/java/ru/akbashev/instacollage/TestActivity.java
+++ InstaCollage/src/main/java/ru/akbashev/instacollage/TestActivity.java
@@ -3,22 +3,6 @@ package ru.akbashev.instacollage;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class TestActivity extends Activity {
 
